125d36853553599270e7e759652006dee3963814
hanep77 <yudishan26@gmail.com>
2025-01-21 21:35:26 +0700

    basic UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,39 @@
 import Image from "next/image";
+import { BiSearch } from "react-icons/bi";
+import { BsHouse, BsHouseFill } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="border-b bg-white sticky top-0">
+        <nav className="flex items-center justify-between h-16 max-w-screen-lg m-auto">
+          <h2 className="text-2xl font-bold text-indigo-600 flex items-center gap-1"><BsHouseFill />Sekosan</h2>
+          <div className="flex gap-2">
+            <button type="button" className="h-8 border-2 border-indigo-600 px-4 rounded-full text-indigo-600 hover:bg-indigo-600 hover:text-white">Sign up</button>
+            <button type="button" className="h-8 bg-indigo-600 text-white px-4 rounded-full">Sign in</button>
+          </div>
+        </nav>
+      </header>
+      <section className="mt-28">
+        <div className="flex max-w-screen-lg m-auto">
+          <div className="w-1/2">
+            <h1 className="text-5xl font-extrabold text-zinc-800">Cari <span className="text-indigo-600">Kosan Idaman</span>, Dapatkan Dengan <span className="text-indigo-600">Gampang</span></h1>
+            <label htmlFor="search" className="flex items-center mt-8 border-2 border-indigo-600 w-80 rounded-full oveflow-hidden">
+              <BiSearch className="ms-4 text-xl text-indigo-600" />
+              <form className="flex-grow flex">
+                <input type="text" id="search" name="search" className="w-full outline-none px-2" placeholder="Cari berdasarkan wilayah" />
+                <button type="button" className="h-8 bg-indigo-600 hover:bg-indigo-700 text-white rounded-e-full px-4">Cari</button>
+              </form>
+            </label>
+          </div>
+          <div className="flex-grow">
+            <Image src={"/assets/house-searching.svg"} alt="house-searcing" width={500} height={500} />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className="min-h-screen">
+
+      </section>
+    </>
   );
 }
